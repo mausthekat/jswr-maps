fc1ce4ae7f50f2e3cf1901097468770e1871d5ff
MauS <mausthekat@users.noreply.github.com>
2026-04-08 13:30:00 -0400

    FEAT: triggers WIP

--- content/jsw-gorgeous/tiles_hazard.tsx
+++ content/jsw-gorgeous/tiles_hazard.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tiles_hazard" tilewidth="8" tileheight="8" tilecount="80" columns="16">
+<tileset version="1.10" tiledversion="1.12.1" name="tiles_hazard" tilewidth="8" tileheight="8" tilecount="80" columns="16">
  <properties>
   <property name="TilesetName" value="Gorgeous"/>
  </properties>
@@ -169,31 +169,55 @@
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="27" duration="200"/>
+   <frame tileid="66" duration="200"/>
+  </animation>
  </tile>
  <tile id="28">
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="28" duration="200"/>
+   <frame tileid="67" duration="200"/>
+  </animation>
  </tile>
  <tile id="29">
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="68" duration="200"/>
+  </animation>
  </tile>
  <tile id="30">
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="30" duration="200"/>
+   <frame tileid="69" duration="200"/>
+  </animation>
  </tile>
  <tile id="31">
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="31" duration="200"/>
+   <frame tileid="70" duration="200"/>
+  </animation>
  </tile>
  <tile id="32">
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="32" duration="200"/>
+   <frame tileid="71" duration="200"/>
+  </animation>
  </tile>
  <tile id="33">
   <properties>
@@ -221,11 +245,19 @@
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="35" duration="200"/>
+   <frame tileid="72" duration="200"/>
+  </animation>
  </tile>
  <tile id="36">
   <properties>
    <property name="TileType" propertytype="TileType" value="hazard"/>
   </properties>
+  <animation>
+   <frame tileid="36" duration="200"/>
+   <frame tileid="73" duration="200"/>
+  </animation>
  </tile>
  <tile id="37">
   <properties>
